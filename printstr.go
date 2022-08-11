ef37ad826d9b7f01e38c3d47eb8031eed2e3c9df
Your Name <you@example.com>
2022-08-11 16:31:19 +0100

    printstr

--- printstr.go
+++ printstr.go
@@ -1,15 +1,12 @@
 package piscine
 
-import(
-	"fmt"
+import (
+	"github.com/01-edu/z01"
 )
 
 func PrintStr(s string) {
-
-
-	for i int = 0; i<s.length;i++;{
-	fmt.Println(s[i])
+	for i := 0; i < len(s); i++ {
+		iyes := s[i]
+		z01.PrintRune(rune(iyes))
 	}
 }
-
-
