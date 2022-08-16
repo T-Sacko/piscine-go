755247cb184339ff2d6b5d8772620697e6b61fca
Titi <sackotiti@gmail.com>
2022-08-16 14:07:06 +0100

    isprime

--- isprime.go
+++ isprime.go
@@ -3,15 +3,18 @@ package piscine
 func IsPrime(nb int) bool {
 	if nb == 1 {
 		return false
-	} else if nb == 2 || nb == 3 {
-		return true
-	} else {
-		limit := Sqrt((nb))
-		for i := 2; i <= int(limit); i++ {
+	}
+
+	if nb <= 0 {
+		return false
+	}
+
+	if nb > 1 {
+		for i := 2; i < nb; i++ {
 			if nb%i == 0 {
 				return false
 			}
 		}
-		return true
 	}
+	return true
 }
