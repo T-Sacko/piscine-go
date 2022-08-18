7cb8a9264b25a5722ed9cf417bcd60781754a792
Titi <sackotiti@gmail.com>
2022-08-18 15:37:44 +0100

    isalpha

---
+++ isaplha.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	str := []byte(s)
+
+	for _, bit := range str {
+		if bit < 97 && bit > 122 || bit < 65 && bit > 90 || bit < 48 && bit > 57 {
+			return false
+		}
+	}
+	return true
+}
