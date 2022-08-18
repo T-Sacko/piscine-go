5b83d2fc35df961c1a9c2b9d5b6f5c877a3c4172
Titi <sackotiti@gmail.com>
2022-08-18 15:45:32 +0100

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -4,9 +4,9 @@ func IsAlpha(s string) bool {
 	str := []byte(s)
 
 	for _, bit := range str {
-		if bit < 97 && bit > 122 || bit < 65 && bit > 90 || bit < 48 && bit > 57 {
-			return false
+		if (bit >= 97 && bit <= 122) || (bit >= 65 && bit <= 90) || (bit >= 48 && bit <= 57) {
+			return true
 		}
 	}
-	return true
+	return false
 }
