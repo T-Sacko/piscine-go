f18954256612aef17912ce1814575ecd2760949b
Titi <sackotiti@gmail.com>
2022-08-18 14:38:10 +0100

    isupper

---
+++ isupper.go
@@ -0,0 +1,25 @@
+package piscine
+
+func IsUpper(s string) bool {
+
+	str := []byte s
+
+	for _, bit := range str {
+		if bit >= 65 && bit<=90{
+			return true
+		}
+		return false
+	}
+
+
+
+
+
+
+
+
+
+
+	
+
+}
